5379c50cf6956c7eb40b00525a07782e2a873b4a
Leandro <lss.91@live.com>
2024-11-29 16:25:48 -0400

    criando barra de navegação

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const App = () => {
     return (
     <div>
+        <Navbar></Navbar>
         <h1>Meu Primeiro App</h1>
     </div>
 );
